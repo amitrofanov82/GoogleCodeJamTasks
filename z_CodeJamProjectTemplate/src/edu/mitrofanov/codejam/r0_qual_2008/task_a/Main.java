9119019dc5d8c4c252ea63a38e1d7f82e0fb34ce
Aleksiej Mitrofanov <aleksiej_mitrofanov@epam.com>
2018-09-17 14:22:04 +0300

    template update

--- z_CodeJamProjectTemplate/src/edu/mitrofanov/codejam/r0_qual_2008/task_a/Main.java
+++ z_CodeJamProjectTemplate/src/edu/mitrofanov/codejam/r0_qual_2008/task_a/Main.java
@@ -1,20 +1,33 @@
 package edu.mitrofanov.codejam.r0_qual_2008.task_a;
 
+import java.io.File;
+import java.util.Scanner;
+
 import edu.mitrofanov.codejam.utils.FileUtils;
 
 public class Main {
-	public final static String INPUT_FILE = "resources/taskA.in";
-	public final static String OUTPUT_FILE = "resources/taskA.out";
+	public final static String INPUT_FILE = "resources/A-test.in";
+	public final static String OUTPUT_FILE = "resources/A-test.out";
+	//public final static String INPUT_FILE = "resources/A-small-practice.in";
+	//public final static String OUTPUT_FILE = "resources/A-small-practice.out";
+	//public final static String INPUT_FILE = "resources/A-large-practice.in";
+	//public final static String OUTPUT_FILE = "resources/A-large-practice.out";
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
-		String[] input = FileUtils.readFileAsStringArray(INPUT_FILE);
-		String[] output = new String[input.length-1];
-		for (int i =1; i< input.length; i++) {
-			output[i-1] = "Case #" + i + ": " + TaskResolver.resolveOneCase(input[i]);
+		Scanner sc = new Scanner(new File(INPUT_FILE));
+		int T = Integer.parseInt(sc.nextLine());
+		String[] output = new String[T];
+		
+		for (int i =0; i< T; i++) {
+			String line = sc.nextLine();
+
+			output[i] = "Case #" + (i+1) + ": " + TaskResolver.resolveOneCase(line);
+			System.out.println(output[i]);
 		}
 		FileUtils.writeStringArrayToFile(output, OUTPUT_FILE);
-		
-		
 	}
+	
+	
 	
 }
